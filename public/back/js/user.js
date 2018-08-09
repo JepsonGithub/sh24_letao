c5f279cbe11cc8417e0d23fc173a9bd66ef36c05
jepson <jepsonpp@qq.com>
2018-08-09 11:19:13 +0800

    用户管理-分页渲染完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -3,23 +3,50 @@
  */
 
 $(function() {
+  var currentPage = 1;
+  var pageSize = 5;
 
   // 1. 一进入页面, 发送ajax请求, 获取用户列表数据, 通过模板引擎渲染
-  $.ajax({
-    type: "get",
-    url: "/user/queryUser",
-    data: {
-      page: 1,
-      pageSize: 5
-    },
-    dataType: "json",
-    success: function( info ) {
-      console.log( info );
-      // template( 模板id, 数据对象 )
-      // 在模板中可以任意使用数据对象中的属性
-      var htmlStr = template( 'tpl', info );
-      $('tbody').html( htmlStr );
-    }
-  })
+  render();
+  function render() {
+    $.ajax({
+      type: "get",
+      url: "/user/queryUser",
+      data: {
+        page: currentPage,
+        pageSize: pageSize
+      },
+      dataType: "json",
+      success: function( info ) {
+        console.log( info );
+        // template( 模板id, 数据对象 )
+        // 在模板中可以任意使用数据对象中的属性
+        var htmlStr = template( 'tpl', info );
+        $('tbody').html( htmlStr );
+
+
+        // 分页初始化
+        $('#paginator').bootstrapPaginator({
+          // 配置 bootstrap 版本
+          bootstrapMajorVersion: 3,
+          // 指定总页数
+          totalPages: Math.ceil( info.total / info.size ),
+          // 当前页
+          currentPage: info.page,
+          // 当页码被点击时调用的回调函数
+          onPageClicked: function( a, b, c, page ) {
+            // 通过 page 获取点击的页码
+
+            // 更新当前页
+            currentPage = page;
+            // 重新渲染
+            render();
+          }
+        });
+      }
+    });
+  }
+
+
 
 });
